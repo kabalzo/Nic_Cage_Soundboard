43589139f68e2c14c2add3e796e0b307380c29c9
Drew Kabala <kabalzo@outlook.com>
2023-05-10 18:33:05 -0500

    added some more commands and phrases

--- Nic_Cage_beta.py
+++ Nic_Cage_beta.py
@@ -16,9 +16,14 @@ def cageQuote():
                    ("I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:", "vampire.mp3"), 
                    ("YOU BITCHES!!! YOU BITCHES!!!", "you_bs.mp3"), 
                    ("I'm just relieved that I'm not as crazy as everyone says...", "crazy.wav"),
-                   ("Put the bunny back in the box :rabbit:", "placeholder"),
+                   ("I said put the bunny back in the box :rabbit:", "placeholder"),
                    ("I'm gonna steal the Declaration of Independence", "placeholder"),
-                   ("I never disrobe before gunplay", "placeholder")
+                   ("I never disrobe before gunplay", "placeholder"),
+                   ("I'll be taking these Huggies and whatever cash you've got", "placeholder"),
+                   ("You know, I could eat a peach for hours :peach:", "placeholder"),
+                   ("Lets ride :skull_crossbones: :motorcycle:", "placeholder"),
+                   ("Sorry Boss But There’s Only Two Men I Trust. One of Them’s Me. And The Other’s Not You :airplane:", "placeholder"),
+                   (":FaceoffCage:", "")
                   ]
     quote = random.randint(0, len(cage_quotes)-1)
     #print(quote)
@@ -36,6 +41,8 @@ async def on_ready():
 async def on_message(message):
     speakComm = "!speak"
     helpComm = "!help"
+    trevorComm = "!trevor"
+    alexComm = "!alex"
     if message.author == client.user:
         print("Debug 2")
         return
@@ -48,7 +55,13 @@ async def on_message(message):
         #print(message.channel)
 
     if helpComm in message.content:
-        await message.channel.send("I see you found the '!help' command. Use the @ towards me with the '!speak' command and I'll say something.")
+        await message.channel.send("Use @ towards me and I'll respond. Commands: !speak, !help, !trevor, !alex")
+
+    if trevorComm in message.content:
+        await message.channel.send("Trevor sucks")
+
+    if alexComm in message.content:
+        await message.channel.send(":AdolfBergquist:")
 
 
 client.run(TOKEN)
