cf121832120fc4f7784da0e63cc292e0b4e25a3c
Drew Kabala <kabalzo@outlook.com>
2023-05-10 21:43:51 -0500

    added more sounds, new gif command, re-wrote some variable/function names, changed the way the tuple is passed back from cageClips (formerly cageQuotes

--- Nic_Cage_beta.py
+++ Nic_Cage_beta.py
@@ -1,4 +1,6 @@
 #This is my Nic Cage bot
+#<:AlexBird:920135756201213953>
+#<:AdolfBergquist:920135114409791578>
 
 import random
 from playsound import playsound
@@ -10,27 +12,27 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-def cageQuote(): 
-    cage_quotes = [
+def cageClips(): 
+    cage_clips = [
                    ("Not the bees! :bee:", "bees.mp3"),
                    ("I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:I'M A VAMPIRE!:man_vampire:", "vampire.mp3"), 
                    ("YOU BITCHES!!! YOU BITCHES!!!", "you_bs.mp3"), 
                    ("I'm just relieved that I'm not as crazy as everyone says...", "crazy.wav"),
-                   ("I said put the bunny back in the box :rabbit:", "placeholder"),
-                   ("I'm gonna steal the Declaration of Independence", "placeholder"),
-                   ("I never disrobe before gunplay", "placeholder"),
-                   ("I'll be taking these Huggies and whatever cash you've got", "placeholder"),
-                   ("You know, I could eat a peach for hours :peach:", "placeholder"),
-                   ("Lets ride :skull_crossbones: :motorcycle:", "placeholder"),
-                   ("Sorry Boss But There’s Only Two Men I Trust. One of Them’s Me. And The Other’s Not You :airplane:", "placeholder"),
-                   (":FaceoffCage:", "")
+                   ("I said put the bunny back in the box :rabbit:", "bunny.mp3"),
+                   ("I'm gonna steal the Declaration of Independence\nhttps://tenor.com/view/national-treasure-benjamin-gates-nicolas-cage-declaration-of-independence-steal-gif-4752081", "steal.wav"),
+                   ("I never disrobe before gunplay", "other.mp3"),
+                   ("I'll be taking these Huggies and whatever cash you've got", "huggies.mp3"),
+                   ("You know, I could eat a peach for hours :peach:", "peach.mp3"),
+                   ("FUUUUUUUUUUUUUUUCCCCCCKKKKK", "fudge.mp3"),
+                   ("Sorry Boss But There’s Only Two Men I Trust. One of Them’s Me. And The Other’s Not You :airplane:", "con-trust.wav"),
+                   ("What am I a fucking retard man?!?", "rtard.mp3")
                   ]
-    quote = random.randint(0, len(cage_quotes)-1)
-    #print(quote)
-    myQuote = cage_quotes[quote][0]
-    print(myQuote)
-    #playsound('sounds/' + cage_quotes[quote][1])
-    return myQuote
+    clip = random.randint(0, len(cage_clips)-1)
+    #print(clip)
+    myClip = cage_clips[clip]
+    print(myClip[0])
+    #playsound('sounds/' + cage_clips[clip][1])
+    return myClip
 
 client = discord.Client(intents=discord.Intents.default())
 @client.event
@@ -43,6 +45,8 @@ async def on_message(message):
     helpComm = "!help"
     trevorComm = "!trevor"
     alexComm = "!alex"
+    gifComm = "!gif"
+
     if message.author == client.user:
         print("Debug 2")
         return
@@ -50,18 +54,38 @@ async def on_message(message):
     if speakComm in message.content:
         #print(type(message.content))
         print("Debug 3")
-        cageQuoteForDiscord = cageQuote()
-        await message.channel.send(cageQuoteForDiscord)
+        cageClipForDiscord = cageClips()
+        await message.channel.send(cageClipForDiscord[0])
+        playsound('sounds/' + cageClipForDiscord[1])
         #print(message.channel)
 
+    #All the commands  go in here#
     if helpComm in message.content:
-        await message.channel.send("Use @ towards me and I'll respond. Commands: !speak, !help, !trevor, !alex")
+        await message.channel.send("Use @ towards me and I'll respond. Commands: !speak, !help, !alex, !gif")
 
     if trevorComm in message.content:
         await message.channel.send("Trevor sucks")
 
     if alexComm in message.content:
-        await message.channel.send(":AdolfBergquist:")
+        alex = ["<:AdolfBergquist:920135114409791578>", "<:AlexBird:920135756201213953>"]
+        r = random.randint(0,1)
+        await message.channel.send(alex[r])
+
+    if gifComm in message.content:
+        r = random.randint(0,3)
+        gifs = ["https://tenor.com/view/nicholas-cage-you-pointing-smoke-gif-14538102",
+                "https://tenor.com/view/nicolas-cage-the-rock-smile-windy-handsome-gif-15812740",
+                "https://tenor.com/view/woo-nick-cage-nicolas-cage-the-unbearable-weight-of-massive-talent-lets-go-gif-25135470",
+                "https://tenor.com/view/national-treasure-benjamin-gates-nicolas-cage-declaration-of-independence-steal-gif-4752081"
+                ]
+        await message.channel.send(gifs[r])
+
+
+
+
+
+
+
 
 
 client.run(TOKEN)
