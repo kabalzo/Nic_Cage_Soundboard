8b2a0ec7918dfcdf2b4b4cf12cd55d19b6759d6c
Drew Kabala <kabalzo@outlook.com>
2023-08-19 23:34:54 -0500

    Added more sounds. Added logic to prevent the same sound from playing back to back

--- Nic_Cage_beta.py
+++ Nic_Cage_beta.py
@@ -12,6 +12,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 intents.message_content = True
+lastInt = -1
 
 NicCageQuotes = [
                 ("Not the bees! :bee:", "bees.mp3"),
@@ -24,7 +25,18 @@ NicCageQuotes = [
                 ("I'll be taking these Huggies and whatever cash you've got", "huggies.mp3"),
                 ("You know, I could eat a peach for hours :peach:", "peach.mp3"),
                 ("FUUUUUUUUUUUUUUUCCCCCCKKKKK", "fudge.mp3"),
-                ("Sorry Boss But There’s Only Two Men I Trust. One of Them’s Me. And The Other’s Not You :airplane:", "con-trust.wav")
+                ("Sorry Boss But There’s Only Two Men I Trust. One of Them’s Me. And The Other’s Not You :airplane:", "con-trust.wav"),
+                ("If I didn't love you so damn much I'd have to kill you bro", "love.mp3"),
+                ("Bring me the big knife I'm gonna cut my throat :knife", "knife.mp3"),
+                ("I am a baaaaaaaaaaad man", "bad.mp3"),
+                ("Because I was made for this sewer baby and I am the King!", "made_for_this.mp3"),
+                ("How in the name of Zeus' BUTTHOLE did you get out of your cell?", "get_out.mp3"),
+                ("I'm like a prickly pear :pear", "pear.mp3"),
+                ("OK OK, FUCK!", "ok-ok.mp3"),
+                ("Have you ever been dragged into the sidewalk and beaten 'till you PISSED BLOOD?!?", "sidewalk.mp3"),
+                ("What do you say we cut the chit chat A-hole", "chitchat.mp3"),
+                ("I'd like to take his face...off...", "faceoff.mp3"),
+                ("Hello?!?", "hello.mp3")
             ]
 
 gifs = [
@@ -33,6 +45,10 @@ gifs = [
         "https://tenor.com/view/woo-nick-cage-nicolas-cage-the-unbearable-weight-of-massive-talent-lets-go-gif-25135470",
         "https://tenor.com/view/national-treasure-benjamin-gates-nicolas-cage-declaration-of-independence-steal-gif-4752081"
         ]
+
+def getRandomInt():
+     i = random.randint(0, len(NicCageQuotes)-1)
+     return i
 
 @bot.event
 async def on_ready():
@@ -46,12 +62,22 @@ async def test(ctx, arg):
 
 @bot.command()
 async def speak(ctx):
+    global lastInt
     #print("Channel ID: " + str(ctx.channel.id))
-    randomQuote = random.randint(0, len(NicCageQuotes)-1)
-    myQuote = NicCageQuotes[randomQuote]
-    print(myQuote[0])
-    #This block is for selecting a random voice clip to play
-    myClip = NicCageQuotes[randomQuote]
+
+    #Prevent the same quote/clip from being played twice in a row
+    myRandomInt = getRandomInt()
+    while (True):
+        if (myRandomInt == lastInt):
+            myRandomInt = getRandomInt()
+        else:
+            lastInt = myRandomInt
+            myQuote = NicCageQuotes[myRandomInt]
+            print(myQuote[0])
+            #This block is for selecting a random voice clip to play
+            myClip = NicCageQuotes[myRandomInt]
+            break
+
     voice = ctx.voice_client.play(discord.FFmpegPCMAudio('./sounds/' + str(myClip[1])))
     await ctx.channel.send(myQuote[0])
 
